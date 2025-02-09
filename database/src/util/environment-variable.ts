494c6fdf62c093b03dc6417bceae7598aff3e637
nishko <commit@nishko.com>
2025-02-09 15:46:15 -0500

    Remove unused files and update database connection string

--- database/src/util/environment-variable.ts
+++
@@ -1,7 +0,0 @@
-export const getEnvironmentVariable = (key: string): string => {
-    const value = process.env[key];
-    if (!value) {
-      throw new Error(`Environment variable ${key} is not set`);
-    }
-    return String(value);
-  };
--- server/dist/index.js
+++
@@ -1,7 +0,0 @@
-import app from './server.js';
-import dotenv from 'dotenv';
-dotenv.config();
-const PORT = process.env['PORT'] || 4000;
-app.listen(PORT, () => {
-    console.log(`🚀 Server is running at http://localhost:${PORT}/graphql`);
-});
